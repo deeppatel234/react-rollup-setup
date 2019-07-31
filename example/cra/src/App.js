9f4ec5bf239e933cadd4f9f123cf79faab3663fe
Deep Patel <deep.patel@agrostar.in>
2019-07-31 14:47:24 +0530

    add create react app example

---
+++ example/cra/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hello from 'react-rollup-setup';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
